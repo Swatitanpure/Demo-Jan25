5cd156faf7ccaf595107d3e69eb00b50c700ff63
Swati Tanpure <swatitanpure55@gmail.com>
2025-02-05 19:10:21 +0530

    Pass dyanamic data

--- Ecomdemo/myapp/views.py
+++ Ecomdemo/myapp/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    context = {
+    'name':'swati',
+    'age':25,
+    'skills':['python','sql','git','django']
+    }
 
-    return render(request,'home.html')
+    return render(request,'home.html',context)
 
